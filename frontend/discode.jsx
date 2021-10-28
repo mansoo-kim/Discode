a3826d360aa7927ec559d5aaa43e4e0153cca708
Mansoo Kim <mansookim09@gmail.com>
2021-10-28 15:12:55 -0400

    Setup npm packages/webpack and add StaticPages#root

---
+++ frontend/discode.jsx
@@ -0,0 +1,6 @@
+import { render } from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById('root');
+  render(<h1>Hello World</h1>, root);
+});
