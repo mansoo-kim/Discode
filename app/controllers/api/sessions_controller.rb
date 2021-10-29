670b897baaf7e1ce6a466b6e0b7ca166062de7f0
Mansoo Kim <mansookim09@gmail.com>
2021-10-28 21:00:55 -0400

    Add Sessions and Users controllers, add common methods to ApplicationController

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,22 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+
+    if @user
+      login!(@user)
+      render 'api/users/show'
+    else
+      render json: ['Username or password is invalid'], status: 401
+    end
+  end
+
+  def destroy
+    if logged_in?
+      logout!
+      render json: {}
+    else
+      render json: ["No user is currently logged in"], status: 404
+    end
+  end
+
+end
