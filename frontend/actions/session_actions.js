6f5a818eecca441aa70a77ba7cf7a82c9242b7fa
Mansoo Kim <mansookim09@gmail.com>
2021-10-28 23:27:02 -0400

    Add actions and reducer for session errors

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,19 +2,21 @@ import * as SessionApiUtil from "../utils/session_api_util";
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
-export const receiveCurrentUser = (currentUser) => {
-  return {
-    type: RECEIVE_CURRENT_USER,
-    currentUser
-  }
-}
+export const receiveCurrentUser = (currentUser) => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
 
-export const logoutCurrentUser = () => {
-  return {
-    type: LOGOUT_CURRENT_USER,
-  }
-}
+export const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER,
+});
+
+export const receiveSessionErrors = (errors) => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+})
 
 export const registerNewUser = (formUser) => (dispatch) => (
   SessionApiUtil.register(formUser)
