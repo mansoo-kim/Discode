c55937f9d9ce1e7a96ea5ff70b8c43e86ddc443d
Mansoo Kim <mansookim09@gmail.com>
2021-10-29 14:25:27 -0400

    Create basic splash component

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,15 +1,15 @@
-import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../utils/route_utils';
+import Splash from './splash/Splash';
 import LoginFormContainer from './session/LoginFormContainer';
 import RegisterFormContainer from './session/RegisterFormContainer';
 
 const App = () => {
   return (
     <div>
-      <Switch>
-        <AuthRoute path='/register' component={RegisterFormContainer} />
-        <AuthRoute path='/login' component={LoginFormContainer} />
-      </Switch>
+      <Route exact path='/' component={Splash} />
+      <AuthRoute path='/register' component={RegisterFormContainer} />
+      <AuthRoute path='/login' component={LoginFormContainer} />
     </div>
   )
 }
---
+++ frontend/components/splash/Splash.jsx
@@ -0,0 +1,9 @@
+const Splash = () => {
+  return (
+    <div>
+      <h3>Splash Screen</h3>
+    </div>
+  )
+}
+
+export default Splash
