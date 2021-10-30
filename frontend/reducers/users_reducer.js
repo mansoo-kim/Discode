8b5237f4ac88fad5c9c5360c8fdce90d5928bed3
Mansoo Kim <mansookim09@gmail.com>
2021-10-30 18:06:45 -0400

    Add members to users state when server is received

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,10 +1,17 @@
 import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
+import { RECEIVE_SERVER } from "../actions/server_actions";
 
 const UsersReducer = (state={}, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
       return Object.assign({}, state, { [action.currentUser.id]: action.currentUser })
+    case RECEIVE_SERVER:
+      const newState = Object.assign({}, state);
+      for (let [id, member] of Object.entries(action.res.users)) {
+        newState[id] = member;
+      }
+      return newState
     default:
       return state;
   }
