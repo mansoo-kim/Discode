3150d29ef00938872f2b01dab6c6461d56e7d89e
Mansoo Kim <mansookim09@gmail.com>
2021-10-31 13:38:18 -0400

    Add RECEIVE_CHANNEL action to channels reducer

--- frontend/actions/channel_actions.js
+++ frontend/actions/channel_actions.js
@@ -2,9 +2,9 @@ import * as ChannelApiUtil from "../utils/channel_api_utils";
 
 export const RECEIVE_CHANNEL = 'RECEIVE_CHANNEL';
 
-export const receiveChannel = (res) => ({
+export const receiveChannel = (channel) => ({
   type: RECEIVE_CHANNEL,
-  res
+  channel
 });
 
 export const createChannel = (channel) => (dispatch) => (
--- frontend/reducers/channels_reducer.js
+++ frontend/reducers/channels_reducer.js
@@ -1,10 +1,13 @@
 import { RECEIVE_SERVER } from "../actions/server_actions";
+import { RECEIVE_CHANNEL } from "../actions/channel_actions";
 
 const ChannelsReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_SERVER:
       return action.res.channels;
+    case RECEIVE_CHANNEL:
+      return Object.assign({}, state, { [action.channel.id]: action.channel })
     default:
       return state;
   }
