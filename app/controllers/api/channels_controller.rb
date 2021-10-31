9ee64a98427e5f1de70a62b05ffc5875f8fa4e28
Mansoo Kim <mansookim09@gmail.com>
2021-10-31 13:29:54 -0400

    Add Channels#create action in controller

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -1,3 +1,15 @@
 class Api::ChannelsController < ApplicationController
+  def create
+    @channel = Server.new(channel_params)
+    @channel.server_id = params[:server_id]
+    if @channel.save
+      render 'api/channels/show'
+    else
+      render json: @channel.errors, status: 422
+    end
+  end
 
+  def channel_params
+    params.require(:channel).permit(:name)
+  end
 end
