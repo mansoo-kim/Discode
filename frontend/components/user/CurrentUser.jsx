905800451630dbfddfdd8074c60e2f7698f1a244
Mansoo Kim <mansookim09@gmail.com>
2021-10-31 15:50:56 -0400

    Add UserSettings page with logout button

--- frontend/components/user/CurrentUser.jsx
+++ frontend/components/user/CurrentUser.jsx
@@ -1,16 +1,33 @@
-import { connect } from 'react-redux'
+import UserSettings from './UserSettings';
+import { useState } from 'react';
 
-const CurrentUser = ({ currentUser }) => {
+const CurrentUser = ({ currentUser, logout }) => {
+  const [showSettings, setShowSettings] = useState(false);
+
+  const toggleSettings = () => (setShowSettings(!showSettings));
+
   return (
     <div>
-      { currentUser.username }
-      #{ currentUser.tag }
+      <div>
+        { currentUser.username }
+        #{ currentUser.tag }
+      </div>
+      <button onClick={toggleSettings}>Settings</button>
+
+      { showSettings && <UserSettings logout={logout} />}
     </div>
   )
 }
+
+import { connect } from 'react-redux';
+import { logout } from '../../actions/session_actions';
 
 const mSTP = (state) => ({
   currentUser: state.entities.users[state.session.id]
 })
 
-export default connect(mSTP)(CurrentUser);
+const mDTP = (dispatch) => ({
+  logout: () => dispatch(logout())
+})
+
+export default connect(mSTP, mDTP)(CurrentUser);
---
+++ frontend/components/user/UserSettings.jsx
@@ -0,0 +1,23 @@
+const UserSettings = ({ logout }) => {
+  return (
+    <div className="settings-container">
+      <div className="settings-left">
+        <div>
+          User Settings
+          <ul>
+            <li>
+              My Account
+            </li>
+            <li onClick={logout}>
+              Log Out
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="settings-right">
+      </div>
+    </div>
+  )
+}
+
+export default UserSettings
