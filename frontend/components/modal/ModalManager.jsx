9a9499a0a8097f15db1990a5419e648cc8d31650
Mansoo Kim <mansookim09@gmail.com>
2021-10-31 10:00:21 -0400

    Connect ModalManager to state

---
+++ frontend/components/modal/ModalManager.jsx
@@ -0,0 +1,21 @@
+const ModalManager = ({ modal, openModal, closeModal }) => {
+  return modal ? (
+    <div>
+
+    </div>
+  ) : null
+}
+
+import { connect } from 'react-redux';
+import { openModal, closeModal } from '../../actions/modal_actions';
+
+const mSTP = (state) => ({
+  modal: state.ui.modal
+});
+
+const mDTP = (dispatch) => ({
+  openModal: (modal) => dispatch(openModal(modal)),
+  closeModal: () => dispatch(closeModal())
+});
+
+export default connect(mSTP, mDTP)(ModalManager);
