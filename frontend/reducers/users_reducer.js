74be23f6ccb14cbf41405059d6e56efc827eb4ee
Mansoo Kim <mansookim09@gmail.com>
2021-10-30 21:35:50 -0400

    Allow UsersReducer to update users when conversation is received

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,5 +1,14 @@
 import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
 import { RECEIVE_SERVER } from "../actions/server_actions";
+import { RECEIVE_CONVERSATION } from "../actions/conversation_actions";
+
+const receiveMembers = (state, action) => {
+  const newState = Object.assign({}, state);
+  for (let [id, member] of Object.entries(action.res.users)) {
+    newState[id] = member;
+  }
+  return newState
+}
 
 const UsersReducer = (state={}, action) => {
   Object.freeze(state);
@@ -7,11 +16,9 @@ const UsersReducer = (state={}, action) => {
     case RECEIVE_CURRENT_USER:
       return Object.assign({}, state, { [action.currentUser.id]: action.currentUser })
     case RECEIVE_SERVER:
-      const newState = Object.assign({}, state);
-      for (let [id, member] of Object.entries(action.res.users)) {
-        newState[id] = member;
-      }
-      return newState
+      return receiveMembers(state, action);
+    case RECEIVE_CONVERSATION:
+      return receiveMembers(state, action);
     default:
       return state;
   }
