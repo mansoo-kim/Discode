867fc6940b8899b8217e11cbd9c87845529f829c
Mansoo Kim <mansookim09@gmail.com>
2021-11-01 15:03:19 -0400

    Update channel name from ChannelSettings component

--- frontend/components/channel/ChannelSettings.jsx
+++ frontend/components/channel/ChannelSettings.jsx
@@ -1,11 +1,15 @@
 import { useForm } from 'react-hook-form';
 
-const ChannelSettings = ({ toggleSettings, channel }) => {
+const ChannelSettings = ({ toggleSettings, channel, updateChannel }) => {
   const { register, formState: { errors, dirtyFields }, handleSubmit } = useForm({
     mode: 'onChange',
     shouldFocusError: false,
     defaultValues: { channelName: channel.name }
   });
+
+  const onSubmit = (data) => (
+    updateChannel(channel.id, { name: data.channelName})
+  );
 
   return (
     <div className="settings-container">
@@ -26,7 +30,7 @@ const ChannelSettings = ({ toggleSettings, channel }) => {
         <div className="settings-pane">
           <h2>OVERVIEW</h2>
 
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
 
             <div>
               <label>CHANNEL NAME</label>
@@ -45,9 +49,10 @@ const ChannelSettings = ({ toggleSettings, channel }) => {
 }
 
 import { connect } from 'react-redux';
+import { updateChannel } from '../../actions/channel_actions';
 
 const mDTP = (dispatch) => ({
-
+  updateChannel: (channelId, channel) => dispatch(updateChannel(channelId, channel))
 })
 
 export default connect(null, mDTP)(ChannelSettings);
