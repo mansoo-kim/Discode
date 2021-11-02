2608157eecbff7c904f6427c58adb7676b817414
Mansoo Kim <mansookim09@gmail.com>
2021-11-02 11:30:50 -0400

    Fix bug by adding newly created servers to currentUser's servers

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,13 +1,15 @@
 import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from "../actions/session_actions";
 import { RECEIVE_USER } from "../actions/user_actions";
+import { RECEIVE_SERVER } from "../actions/server_actions";
 
 const _nullSession = {
   id: null,
-  name: null,
+  username: null,
   email: null,
   tag: null,
   servers: [],
-  conversations: []
+  conversations: [],
+  updatedAt: null
 }
 
 const SessionReducer = (state = _nullSession, action) => {
@@ -19,6 +21,13 @@ const SessionReducer = (state = _nullSession, action) => {
       return _nullSession;
     case RECEIVE_USER:
       if (action.user.id === state.id) return action.user;
+      return state;
+    case RECEIVE_SERVER:
+      if (action.res.server.ownerId === state.id && !state.servers.includes(action.res.server.id)) {
+        const newState = Object.assign({}, state);
+        newState.servers.push(action.res.server.id);
+        return newState
+      }
       return state;
     default:
       return state;
