81ed43b30c5594a130b06d1349dd6a35ffb7617e
Mansoo Kim <mansookim09@gmail.com>
2021-11-03 15:48:00 -0400

    Create ChatChannel and mount ActionCable

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   root to: 'static_pages#root'
 
+  mount ActionCable.server, at: '/cable'
+
   namespace :api, defaults: { format: :json } do
     resource :session, only: [:create, :destroy]
     resources :users, only: [:create, :update, :show]
