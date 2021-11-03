c81d7f0e60635344899e6b085e810b726239c060
Mansoo Kim <mansookim09@gmail.com>
2021-11-03 14:06:38 -0400

    Refactor ChannelSettings

--- frontend/components/channel/ChannelSettings.jsx
+++ frontend/components/channel/ChannelSettings.jsx
@@ -1,16 +1,106 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+const ChannelSettings = ({ toggleSettings, channel, updateChannel, deleteChannel, history }) => {
+  if (!channel) return null;
+
+  const { register, formState: { errors, isDirty }, watch, reset, handleSubmit } = useForm({
+    shouldFocusError: false,
+    defaultValues: { channelName: channel.name }
+  });
+
+  const [showRed, setShowRed] = useState(false);
+  const watchName = watch("channelName");
+
+  const handleDelete = () => {
+    deleteChannel(channel.id)
+      .then(() => toggleSettings(null))
+      .then(() => {
+        if (history.location.pathname !== `/channels/${channel.serverId}`) history.push(`/channels/${channel.serverId}`)
+      });
+  }
+
+  const checkThenExit = () => {
+    if (isDirty) {
+      setShowRed(true);
+    } else {
+      toggleSettings(null);
+    }
+  }
+
+  const onSubmit = (data) => {
+    const formData = { name: data.channelName};
+
+    updateChannel(channel.id, formData)
+      .then(() => {
+        setShowRed(false);
+        reset({ channelName: watchName});
+      },
+      () => {})
+  };
+
+  const prompt = (
+    <div className={`save-prompt ${ showRed ? 'error-input' : ''}`}>
+      Careful - you have unsaved changes!
+      <button onClick={() => {
+        setShowRed(false);
+        reset();
+      }}>
+        Reset
+      </button>
+      <button>Save Changes</button>
+    </div>
+  )
+
+  return (
+    <div className="settings-container">
+      <div className="settings-left">
+        <div>
+          <div>
+            # { watchName } TEXT CHANNELS
+          </div>
+          <ul>
+            <li>
+              Overview
+            </li>
+            <li onClick={handleDelete}>
+              Delete Channel
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="settings-right">
+        <div className="settings-pane">
+          <h2>OVERVIEW</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <label>CHANNEL NAME</label>
+              <input type="text" placeholder={channel.name} {...register("channelName", { required: "This field is required" })} />
+              { errors.channelName?.message }
+            </div>
+            { (isDirty ) && prompt }
+          </form>
+        </div>
+      </div>
+
+      <div className="close-settings">
+        <button onClick={checkThenExit}>X</button>
+      </div>
+    </div>
+  )
+}
+
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { updateChannel, deleteChannel } from '../../actions/channel_actions';
-import SettingsPage from '../settings/SettingsPage';
 
 const mSTP = (state, ownProps) => ({
-  subject: state.entities.channels[ownProps.channelId],
-  type: "Channel"
+  channel: state.entities.channels[ownProps.channelId],
 })
 
 const mDTP = (dispatch) => ({
-  updateSubject: (channelId, channel) => dispatch(updateChannel(channelId, channel)),
-  deleteSubject: (channelId) => dispatch(deleteChannel(channelId))
+  updateChannel: (channelId, channel) => dispatch(updateChannel(channelId, channel)),
+  deleteChannel: (channelId) => dispatch(deleteChannel(channelId))
 });
 
-export default withRouter(connect(mSTP, mDTP)(SettingsPage));
+export default withRouter(connect(mSTP, mDTP)(ChannelSettings));
