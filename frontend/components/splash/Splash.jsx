cd7102ad0a62da1b5cd4af3a645d4bb3b2c518b8
Mansoo Kim <mansookim09@gmail.com>
2021-11-04 19:16:28 -0400

    Style splash navbar

--- frontend/components/splash/Splash.jsx
+++ frontend/components/splash/Splash.jsx
@@ -1,9 +1,36 @@
-const Splash = () => {
+import { Link } from 'react-router-dom';
+
+const Splash = ({ loggedIn }) => {
+  const appLink = loggedIn ? (
+    <Link to='/channels/@me'>Open Discode</Link>
+  ) : (
+    <Link to='/login'>Login</Link>
+  )
+
   return (
-    <div>
-      <h3>Splash Screen</h3>
+    <div className="splash">
+      <nav className="splash-nav">
+        <div className="splash-logo">
+          <img src='https://raw.githubusercontent.com/mansookim/Discode/main/app/assets/images/icon_clyde_white_RGB.png' className="splash-logo-icon" />
+          <span>Discode</span>
+        </div>
+
+        <ul>
+          <li><a href="#">Github</a></li>
+          <li><a href="#">LinkedIn</a></li>
+          <li><a href="#">Portfolio</a></li>
+        </ul>
+        <div className="app-link">{ appLink }</div>
+      </nav>
     </div>
   )
 }
 
-export default Splash
+import { connect } from 'react-redux';
+
+const mSTP = (state) => ({
+  loggedIn: Boolean(state.session.id)
+});
+
+
+export default connect(mSTP)(Splash);
