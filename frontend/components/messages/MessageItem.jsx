2cd8daacca8e727803a7a9a03be9d5cef3672ec1
Mansoo Kim <mansookim09@gmail.com>
2021-11-04 12:18:22 -0400

    Show/toggle edit input for messages

--- frontend/components/messages/MessageItem.jsx
+++ frontend/components/messages/MessageItem.jsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
+
 const MessageItem = ({ message, sender, currentUserId }) => {
+  const [showEdit, setShowEdit] = useState(false);
+  const [body, setBody] = useState(message.body);
+
+  const toggleEdit = () => setShowEdit(!showEdit);
+
   const imgSrc = sender.pfpUrl || 'https://raw.githubusercontent.com/mansookim/Discode/main/app/assets/images/icon_clyde_white_RGB.png'
+
+  const editInput = (
+    <form>
+      <input type="text" value={body} onChange={(e) => setBody(e.currentTarget.value)} />
+    </form>
+  )
 
   return (
     <div className="message-item">
       <div className="message-pfp">
         <img src={imgSrc} className="pfp" />
       </div>
-      { message.body }
-      { currentUserId === sender.id && <button>Edit</button>}
+      { showEdit ? editInput : message.body }
+      { currentUserId === sender.id && <button onClick={toggleEdit}>Edit</button>}
     </div>
   )
 }
