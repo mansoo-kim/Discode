6b4b675018c1c8ef8f6db95005454efd8c868de4
Mansoo Kim <mansookim09@gmail.com>
2021-11-06 11:34:58 -0400

    Style static CurrentUser and CC Header

--- frontend/components/channel/ChannelIndexItem.jsx
+++ frontend/components/channel/ChannelIndexItem.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
-import { HiOutlineHashtag } from 'react-icons/hi';
+import { RiHashtag } from 'react-icons/ri';
 import { BsFillGearFill } from 'react-icons/bs';
 
 const ChannelIndexItem = ({ channel, isOwner, toggleSettings }) => {
   return (
     <div className="cc-index-item">
       <Link className="channel-button" to={`/channels/${channel.serverId}/${channel.id}`}>
-          <div><HiOutlineHashtag size={20} /></div>
+          <div><RiHashtag size={20} /></div>
           <div>{ channel.name }</div>
       </Link>
       { isOwner && <div className="new-channel-button" tabIndex="0" onClick={() => toggleSettings(channel.id)}><BsFillGearFill /></div>}
--- frontend/components/user/CurrentUser.jsx
+++ frontend/components/user/CurrentUser.jsx
@@ -1,20 +1,30 @@
 import { useState } from 'react';
 import UserSettings from './UserSettings';
+import UserPfp from './UserPfp';
+import { BsFillGearFill } from 'react-icons/bs';
+import { RiHashtag } from 'react-icons/ri';
 
 const CurrentUser = ({ currentUser }) => {
   const [showSettings, setShowSettings] = useState(false);
   const toggleSettings = () => (setShowSettings(!showSettings));
 
-  const imgSrc = currentUser.pfpUrl || 'https://raw.githubusercontent.com/mansookim/Discode/main/app/assets/images/icon_clyde_white_RGB.png'
-
   return (
-    <div className="current-user">
-      <div>
-        <img src={imgSrc} className="pfp" />
-        { currentUser.username }
-        #{ currentUser.tag }
+    <div className="current-user-container">
+      <div className="current-user">
+        <div className="current-user-info">
+          <UserPfp user={currentUser} />
+          <div>
+            <div className="username">{ currentUser.username }</div>
+            <div className="tag">
+              <RiHashtag size={12} />{ currentUser.tag }
+            </div>
+          </div>
+        </div>
+
+        <div tabIndex="0" onClick={toggleSettings}>
+          <BsFillGearFill size={20} />
+        </div>
       </div>
-      <button onClick={toggleSettings}>Settings</button>
 
       { showSettings && <UserSettings currentUser={currentUser} toggleSettings={toggleSettings} />}
     </div>
