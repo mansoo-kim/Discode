647aa61da148b19d2b3ea32dc92e177f558f312a
Mansoo Kim <mansookim09@gmail.com>
2021-11-07 12:33:45 -0500

    Adjust channel index spacing

--- frontend/components/channel/ChannelIndexItem.jsx
+++ frontend/components/channel/ChannelIndexItem.jsx
@@ -6,7 +6,7 @@ const ChannelIndexItem = ({ channel, isOwner, activeChannelId, toggleSettings })
   return (
     <div className={`cc-index-item ${activeChannelId === channel.id ? 'active-channel' : ''}`}>
       <Link className="channel-button" to={`/channels/${channel.serverId}/${channel.id}`}>
-          <div><FaHashtag size={16} /></div>
+          <div><FaHashtag size={18} /></div>
           <div>{ channel.name }</div>
       </Link>
       { isOwner && <div className="new-channel-button" onClick={() => toggleSettings(channel.id)}><BsFillGearFill size={14} /></div>}
