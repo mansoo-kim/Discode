4b34d1a7dcfe10696dace3e3e6229ff306a5b871
Mansoo Kim <mansookim09@gmail.com>
2021-11-07 23:29:01 -0500

    Fix bug with logout modal not closing

--- frontend/components/modal/LogOutModal.jsx
+++ frontend/components/modal/LogOutModal.jsx
@@ -1,4 +1,10 @@
 const LogOutModal = ({ closeModal, logout}) => {
+
+  const handleLogout = () => {
+    logout()
+      .then(() => closeModal());
+  }
+
   return (
     <div className="modal">
       <button onClick={closeModal}>X</button>
@@ -8,7 +14,7 @@ const LogOutModal = ({ closeModal, logout}) => {
         <p>Are you sure you want to logout?</p>
 
         <button type="button" onClick={closeModal}>Cancel</button>
-        <button type="button" onClick={logout}>
+        <button type="button" onClick={handleLogout}>
           Log Out
         </button>
       </form>
