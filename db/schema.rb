58bedf8dbca096bf1b7bfbdb3f69037ad82f2fc6
Mansoo Kim <mansookim09@gmail.com>
2021-11-09 10:43:56 -0500

    Create friendships table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_213834) do
+ActiveRecord::Schema.define(version: 2021_11_09_153557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,16 @@ ActiveRecord::Schema.define(version: 2021_11_02_213834) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "friend_id", null: false
+    t.integer "status", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true
+    t.index ["user_id", "status"], name: "index_friendships_on_user_id_and_status"
   end
 
   create_table "memberships", force: :cascade do |t|
