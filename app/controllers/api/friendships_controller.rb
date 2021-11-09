916847196db60f0183561e09a2927e0ffd3a2afe
Mansoo Kim <mansookim09@gmail.com>
2021-11-09 18:09:17 -0500

    Add Update method to Friendships controller

--- app/controllers/api/friendships_controller.rb
+++ app/controllers/api/friendships_controller.rb
@@ -6,10 +6,6 @@ class Api::FriendshipsController < ApplicationController
   end
 
   def create
-    # @user = current_user
-    # @friend = User.find_by(id: friendship_params[:friend_id])
-    # @user.outgoings.append(@friend)
-    # @friend.incomings.append(@user)
     @friendship = Friendship.create(user_id: current_user.id, friend_id: friendship_params[:friend_id], status: 0)
     Friendship.create(friend_id: current_user.id, user_id: friendship_params[:friend_id], status: 1)
 
@@ -17,8 +13,14 @@ class Api::FriendshipsController < ApplicationController
   end
 
   def update
+    @friendship = Friendship.find_by(user_id: friendship_params[:user_id], friend_id: friendship_params[:friend_id])
+    friendship = Friendship.find_by(user_id: friendship_params[:friend_id], friend_id: current_user.id)
 
-
+    if @friendship.update(status: 2) && friendship.update(status: 2)
+      render 'api/friendships/show'
+    else
+      render json: @friendship.errors, status: 422
+    end
   end
 
   def destroy
