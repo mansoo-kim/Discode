b83f4d32c805f762d583fbb8180edc523d365bbc
Mansoo Kim <mansookim09@gmail.com>
2021-11-09 18:15:39 -0500

    Add Friendship crud action creators and api utils

--- frontend/actions/friendship_actions.js
+++ frontend/actions/friendship_actions.js
@@ -1,8 +1,33 @@
 import * as FriendshipApiUtil from "../utils/friendship_api_utils";
 import { receiveUsers } from "./user_actions";
 
+export const CREATE_FRIENDSHIP = 'CREATE_FRIENDSHIP';
+export const UPDATE_FRIENDSHIP = 'UPDATE_FRIENDSHIP';
+export const REMOVE_FRIENDSHIP = 'REMOVE_FRIENDSHIP';
+
 export const requestFriendships = () => (dispatch) => (
   FriendshipApiUtil.requestFriendships()
+    .then(
+      (users) => dispatch(receiveUsers(users))
+    )
+);
+
+export const createFriendship = (ids) => (dispatch) => (
+  FriendshipApiUtil.createFriendship(ids)
+    .then(
+      (users) => dispatch(receiveUsers(users))
+    )
+);
+
+export const updateFriendship = (ids) => (dispatch) => (
+  FriendshipApiUtil.updateFriendship(ids)
+    .then(
+      (users) => dispatch(receiveUsers(users))
+    )
+);
+
+export const deleteFriendship = (ids) => (dispatch) => (
+  FriendshipApiUtil.deleteFriendship(ids)
     .then(
       (users) => dispatch(receiveUsers(users))
     )
--- frontend/utils/friendship_api_utils.js
+++ frontend/utils/friendship_api_utils.js
@@ -4,3 +4,27 @@ export const requestFriendships = () => (
     method: 'GET'
   })
 );
+
+export const createFriendship = (ids) => (
+  $.ajax({
+    url: `/api/friendships`,
+    method: 'POST',
+    data: { friendship: ids }
+  })
+);
+
+export const updateFriendship = (ids) => (
+  $.ajax({
+    url: `/api/friendships`,
+    method: 'PATCH',
+    data: { friendship: ids }
+  })
+);
+
+export const deleteFriendship = (ids) => (
+  $.ajax({
+    url: `/api/friendships`,
+    method: 'DELETE',
+    data: { friendship: ids }
+  })
+);
