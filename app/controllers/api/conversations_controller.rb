08d23fb7b3630fd1178f454beb50c5fd8cb6d963
Mansoo Kim <mansookim09@gmail.com>
2021-11-10 11:29:55 -0500

    Add Conversations controller create method

--- app/controllers/api/conversations_controller.rb
+++ app/controllers/api/conversations_controller.rb
@@ -8,4 +8,21 @@ class Api::ConversationsController < ApplicationController
     @conversation = current_user.conversations.includes(:members).find_by(id: params[:id])
     render 'api/conversations/show'
   end
+
+  def create
+    @conversation = Conversation.new();
+    @conversation.member_ids = conversation_params[:member_ids]
+
+    if @conversation.save
+      render 'api/conversations/show'
+    else
+      render json: @conversation.errors, status: 422
+    end
+  end
+
+  private
+
+  def conversation_params
+    params.require(:conversation).permit(member_ids: [])
+  end
 end
