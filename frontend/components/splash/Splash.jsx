9122f6b900e65b8477d0c40f2179d9aff4878c12
Mansoo Kim <mansookim09@gmail.com>
2021-11-29 18:43:13 -0500

    Add background image to splash top

--- frontend/components/splash/Splash.jsx
+++ frontend/components/splash/Splash.jsx
@@ -5,47 +5,54 @@ const Splash = ({ loggedIn }) => {
 
   return (
     <div className="splash">
-      <header>
-        <nav className="splash-nav">
-          <div className="splash-logo">
-            <img src='https://raw.githubusercontent.com/mansoo-kim/Discode/main/app/assets/images/icon_clyde_white_RGB.png' className="splash-logo-icon" />
-            <span>Discode</span>
-          </div>
+      <div className="blue">
 
-          <ul>
-            <li><a href="https://github.com/mansoo-kim/Discode" target="_blank">Github</a></li>
-            <li><a href="https://www.linkedin.com/in/mansoo-kim/" target="_blank">LinkedIn</a></li>
-            <li><a href="https://mansoo.net/" target="_blank">Portfolio</a></li>
-          </ul>
-          <div className="app-link">
-            <Link className="button" to='/login'>{loggedIn ? 'Open Discode' : 'Login'}</Link>
-          </div>
-        </nav>
-      </header>
-
-      <main className="splash-main">
-
-        <div>
-
-          <h1 className="main-header">
-            IMAGINE A PLACE...
-          </h1>
-
-          <div className="description">
-            ...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.
-          </div>
-
-          <div className="header-buttons">
-            <div className="splash-button download button">
-              <MdOutlineFileDownload className="download-icon" size={25} />
-              Download for { window.navigator.platform.includes('Win') ? 'Windows' : 'Mac'}
-            </div>
-            <Link className="splash-button open button" to='/login'>Open Discord in your browser</Link>
-          </div>
-
+        <div className="main-images">
+          <div className="background"></div>
+          <div className="left"></div>
+          <div className="right"></div>
         </div>
 
-      </main>
+        <header>
+          <nav className="splash-nav">
+            <div className="splash-logo">
+              <img src='https://raw.githubusercontent.com/mansoo-kim/Discode/main/app/assets/images/icon_clyde_white_RGB.png' className="splash-logo-icon" />
+              <span>Discode</span>
+            </div>
+
+            <ul>
+              <li><a href="https://github.com/mansoo-kim/Discode" target="_blank">Github</a></li>
+              <li><a href="https://www.linkedin.com/in/mansoo-kim/" target="_blank">LinkedIn</a></li>
+              <li><a href="https://mansoo.net/" target="_blank">Portfolio</a></li>
+            </ul>
+            <div className="app-link">
+              <Link className="button" to='/login'>{loggedIn ? 'Open Discode' : 'Login'}</Link>
+            </div>
+          </nav>
+        </header>
+
+        <main className="splash-main">
+          <div className="main-content">
+
+            <h1 className="main-header">
+              IMAGINE A PLACE...
+            </h1>
+
+            <div className="description">
+              ...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.
+            </div>
+
+            <div className="header-buttons">
+              <div className="splash-button download button">
+                <MdOutlineFileDownload className="download-icon" size={25} />
+                Download for { window.navigator.platform.includes('Win') ? 'Windows' : 'Mac'}
+              </div>
+              <Link className="splash-button open button" to='/login'>Open Discord in your browser</Link>
+            </div>
+
+          </div>
+        </main>
+      </div>
 
 
     </div>
